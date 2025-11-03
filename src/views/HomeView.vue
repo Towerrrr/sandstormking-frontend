b8249dc068845a236514327e57566fecd923543c
Towerrrr <1611993104@qq.com>
2025-11-03 22:23:25 +0800

    feat: 自动生成 api 请求代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import { healthUsingGet } from '@/api/mainController'
+
+healthUsingGet().then((res) => {
+  console.log(res)
+})
 </script>
 
 <template>
